8d878cfbb691165500a418a443fb7fdeced0827a
chen-judge <31342901+chen-judge@users.noreply.github.com>
2025-03-02 22:31:15 +0800

    code

--- run_r2r/zero_shot.bash
+++ run_r2r/zero_shot.bash
@@ -1,11 +1,8 @@
 export GLOG_minloglevel=2
 export MAGNUM_LOG=quiet
 
-split="val_unseen_sample100"
-#split="val_unseen"
-#data_path=data/logs/llm/0730_no_SAM
-#data_path=data/logs/llm/release_test
-data_path=data/logs/llm/06072_GPT4o_ZS_ThreeeWaypoints_graph_baseline_semantic_multi_start_traj5_sample100
+split="val_unseen"
+data_path=data/logs/llm/release_test
 
 flag2="--exp_name release_r2r
       --run-type eval
@@ -15,7 +12,7 @@ flag2="--exp_name release_r2r
       GPU_NUMBERS 1
       NUM_ENVIRONMENTS 1
       TASK_CONFIG.SIMULATOR.HABITAT_SIM_V0.ALLOW_SLIDING True
-      EVAL.CKPT_PATH_DIR data/logs/checkpoints/0717_all_train_ckpt_RGBD/ckpt.iter12000.pth
+      EVAL.CKPT_PATH_DIR data/logs/checkpoints/release_r2r/ckpt.iter12000.pth
       IL.back_algo control
       IL.max_traj_len 5
       LLM_DIR ${data_path}
